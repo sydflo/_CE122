1e5598c968d0dce9e70f23fa865f2aa3062de034
sydflo <sydneyflower125@gmail.com>
2025-01-26 13:14:49 -0700

    Create FLOWERsydneyA2.cpp

---
+++ Homework/FLOWERsydneyA2.cpp
@@ -0,0 +1,58 @@
+/*
+Sydney FLower
+CE 122
+Homework 2
+Due: 29 January 2025
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 5.6;
+	cout << x;
+	return (0);
+
+}
+
+
+/*
+
+Be sure to follow the submission instructions for this assignment – that’s
+worth (5 points).
+1. (5 points) Write code that asks a user to input three decimal numbers. The program
+should also add those three numbers, convert that sum to an integer, and print the
+result.
+2. (5 points) Write code that requests a weight input in kilograms from a user and outputs
+the weight in pounds.
+3. (5 points) Write code which takes a user’s full name as input and displays a welcome
+message. As a possible example:
+
+$What’s your name?
+$Matt Welz
+$Nice to meet you Matt Welz! Thanks for using my code.
+
+4. (5 points) For this problem, just enter your answer in comments with the rest of the
+problems above. Suppose that height is a double variable and ch is a char variable,
+and name is a string variable. What are the values of height and name after the
+following statements execute:
+
+
+cin >> height;
+cin.get(ch);
+getline(cin, name);
+
+if the input is
+
+6.3 Jose Silva
+
+compared to
+
+6.3
+Jose Silva
+1
+
+
+*/
